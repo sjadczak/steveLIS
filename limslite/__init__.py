6d63acaab84a295a470de4f41f0d365ef8554433
Steve Jadczak <stevejadczak@gmail.com>
2018-06-07 11:43:03 -0400

    updates to ui
    Signed-off-by: Steve Jadczak <stevejadczak@gmail.com>

---
+++ limslite/__init__.py
@@ -0,0 +1,1 @@
+from . import limslite_server
--- limslite/limslite_server.py
+++ limslite/limslite_server.py
@@ -41,9 +41,11 @@ def fetch_results():
     global results
     global runid
     runid = request.args.get('run')
+    logger.info('Fetching results for run {}...'.format(runid))
     with CursorFromPool() as cur:
         cur.execute("SELECT * FROM results_file(%s)", (str(runid),))
         results = [result for result in cur.fetchall()]
+    logger.info('Run {} results fetched'.format(runid))
     return render_template('index.html', run_list=runs, results=results, runid=runid)
 
 
@@ -52,15 +54,20 @@ def download():
     if not os.path.isdir('tmp/'):
         os.makedirs('tmp/')
     time = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
-    with open('tmp/{}_run-{}-results.csv'.format(time, runid), 'w', newline='') as f:
+    logger.debug('Opening file to write results to...')
+    filename = 'tmp/{}_run-{}-results.csv'.format(time, runid)
+    with open(filename, 'w', newline='') as f:
         writer = csv.writer(f, delimiter=',')
         writer.writerow(('assay', 'instrument_sw', 'sample_role', 'sample_type', 'sample_id', 'result', 'units',
                          'result_status', 'username,', 'flags', 'cntrl_cts', 'comments', 'dwp_id', 'mwp_id',
                          'mwp_position', 'start_ts', 'end_ts'))
+        logger.info('Writing results to file({})'.format(filename))
         writer.writerows(results)
-    return send_file('tmp/{}_run-{}-results.csv'.format(time, runid),
+    logger.info('Results CSV saved at {}'.format(filename))
+    logger.info('Sending {} for download'.format(filename))
+    return send_file(filename,
                      mimetype='text/csv',
-                     attachment_filename='{}_run-{}-results.csv'.format(time, runid),
+                     attachment_filename=filename,
                      as_attachment=True)
 
 
